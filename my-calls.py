d1ec87e01878b5f0bfc016c4eb3894179a886078
Bradford <phillip.g.bradford@gmail.com>
2024-10-08 14:39:56 -0400

    update

--- my-calls.py
+++ my-calls.py
@@ -1,6 +1,6 @@
 import httpx
 
-url = "https://bug-free-system-q7p7wx6j7g629vj7-5000.app.github.dev/"
+url = "https://cautious-doodle-pjpjx694jxvcr944-5000.app.github.dev/"
 
 response = httpx.get(url)
 print(response.status_code)
@@ -13,13 +13,14 @@ print(response.status_code)
 print(response.text)
 
 mydata = {
-    "text": "Hello!",
+    "text": "Hello Phil!",
     "param2": "Making a POST request",
     "body": "my own value"
 }
 
 # A POST request to the API
-response = httpx.post(url, data=mydata)
+response = httpx.post(url + "echo", data=mydata)
 
 # Print the response
+print(response.status_code)
 print(response.text) 
